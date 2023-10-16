6d523692386704191a92bef2db3f156c43f6a1f8
Gabrieljerad <gabrieljerad448@gmail.com>
2023-10-16 18:19:15 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-console.log('✅ㅤIniciando...');
+console.log('✅ㅤsuccessfully logged in...');
 import {join, dirname} from 'path';
 import {createRequire} from 'module';
 import {fileURLToPath} from 'url';
@@ -13,11 +13,11 @@ const {name, author} = require(join(__dirname, './package.json'));
 const {say} = cfonts;
 const rl = createInterface(process.stdin, process.stdout);
 
-say('Mystic - Bot\nWhatsApp Bot MD', {
+say('Gabby - Bot\nWhatsApp Bot MD', {
   font: 'chrome',
   align: 'center',
   gradient: ['red', 'magenta']});
-say(`Bot creado por Bruno Sobrino`, {
+say(`Bot created by Gabby`, {
   font: 'console',
   align: 'center',
   gradient: ['red', 'magenta']});
@@ -32,7 +32,7 @@ function start(file) {
   isRunning = true;
   const args = [join(__dirname, file), ...process.argv.slice(2)];
 
-  say('Ajuste la pantalla para escanear el codigo QR', {
+  say('scan again', {
     font: 'console',
     align: 'center',
     gradient: ['red', 'magenta']});
@@ -56,7 +56,7 @@ function start(file) {
   });
   p.on('exit', (_, code) => {
     isRunning = false;
-    console.error('❎ㅤOcurrio un error inesperado:', code);
+    console.error('❎ㅤan error occured:', code);
 
     p.process.kill();
     isRunning = false;
